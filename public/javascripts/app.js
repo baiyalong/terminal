e1883d19d732267b63b92755e5acd568910a3902
baiyalong <baiyalong@gmail.com>
2017-02-21 21:54:17 +0800

    socket

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,29 @@
 console.log('app.js')
+
+id = window
+    .location
+    .search
+    .split('=')[1]
+console.log(id)
+
+// var term = new Terminal(); term.open(document.getElementById('#terminal'));
+// term.write('Hello from \033[1;3;31mxterm.js\033[0m $ ') term.fit()
+
+var socket = io.connect();
+// socket.on('news', function (data) {     console.log(data); socket.emit('my
+// other event', {my: 'data'}); });
+socket.on('connect', function () {
+    term = new Terminal();
+
+    term.on('data', function (data) {
+        socket.emit('req', data);
+    });
+
+    term.open(document.getElementById('terminal'));
+    // term.resize(width, height);
+    term.setOption('cursorBlink', true);
+
+    socket.on('resp', function (data) {
+        term.write(data)
+    })
+})
--- socket.js
+++ socket.js
@@ -1,9 +1,11 @@
 module.exports = io => {
     io
         .on('connection', function (socket) {
-            socket.emit('news', {hello: 'world'});
-            socket.on('my other event', function (data) {
-                console.log(data);
-            });
+
+            socket
+                .on('req', function (data) {
+                    console.log(data)
+                    socket.emit('resp', 'resp' + data);
+                });
         });
 }
